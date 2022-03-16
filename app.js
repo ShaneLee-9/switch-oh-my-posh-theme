ba2b6eee28645b4591204a5e7806a9382044a655
ShaneLee-9 <30720287+ShaneLee-9@users.noreply.github.com>
2022-03-16 22:00:29 +0800

    ⚡️ update the command to Oh My Posh version 7.40.0

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
 const os = require('os')
 const path = require('path')
 const fs = require('fs')
-
-// console.log(os.homedir())
 const homeDir = os.homedir() // 用户根目录
 
 // 获取主题文件
 function getThemes () {
-  const ohMyPoshDir = path.join(homeDir, '/Documents/PowerShell/Modules/oh-my-posh') // on-my-posh目录
-  const ohMyPoshVerDirs = fs.readdirSync(ohMyPoshDir) // oh-my-posh版本目录
-  const ohMyPoshLatestVer = ohMyPoshVerDirs[ohMyPoshVerDirs.length - 1] // oh-my-posh最新版本
-  const themeDir = path.join(ohMyPoshDir, ohMyPoshLatestVer, 'themes') // 主题所在目录
-  const themes = fs.readdirSync(themeDir) // 所有主题文件
+  const themeDir = path.join(homeDir, '/AppData/Local/oh-my-posh/themes') // 主题所在目录
 
-  return themes.map(cur => cur.replace('.omp.json', '')); // 去除主题文件后缀
+  // 返回所有主题文件
+  return fs.readdirSync(themeDir);
 }
 
 // 设置主题
 function setTheme() {
   const psProfilePath = 'Documents/PowerShell/Microsoft.PowerShell_profile.ps1'; // powershell配置文件
   const psProfile = fs.readFileSync(path.join(homeDir, psProfilePath), 'utf-8')
-  const themeNameReg = /(Set-PoshPrompt\s-Theme\s)(.*)/ // 主题名匹配规则
+  const themeNameReg = /(oh-my-posh\sprompt\sinit\spwsh\s--config\s"~\/AppData\/Local\/oh-my-posh\/themes\/)(.*?)("\s\|\sInvoke-Expression)/ // 主题名匹配规则
   const allThemes = getThemes()
-  // const curTheme = psProfile.match(themeNameReg)[2]
-  // const nextThemeIndex = allThemes.indexOf(curTheme) + 1 >= allThemes.length ? 0 : allThemes.indexOf(curTheme) + 1
-  // const nextTheme = allThemes[nextThemeIndex]
 
   const nextTheme = allThemes[Math.floor(Math.random() * allThemes.length)]
-  const newPSProfile = psProfile.replace(themeNameReg, "$1" + nextTheme) // 替换主题后的profile文件
+  const newPSProfile = psProfile.replace(themeNameReg, "$1" + nextTheme + "$3"); // 替换主题后的profile文件
 
   fs.writeFileSync(path.join(homeDir, psProfilePath), newPSProfile, 'utf-8')
   console.log('设置成功');
