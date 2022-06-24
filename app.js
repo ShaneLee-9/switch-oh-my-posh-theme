cc3ac2a4c87288a8da2635b971d98ea823430907
ShaneLee-9 <30720287+ShaneLee-9@users.noreply.github.com>
2022-06-25 00:50:42 +0800

    ♻️ rewrite the path to the themes folder

--- app.js
+++ app.js
@@ -3,23 +3,14 @@ const path = require('path')
 const fs = require('fs')
 const homeDir = os.homedir() // 用户根目录
 
-// 获取主题文件
-function getThemes () {
-  const themeDir = path.join(homeDir, '/AppData/Local/oh-my-posh/themes') // 主题所在目录
-
-  // 返回所有主题文件
-  return fs.readdirSync(themeDir);
-}
-
 // 设置主题
 function setTheme() {
   const psProfilePath = 'Documents/PowerShell/Microsoft.PowerShell_profile.ps1'; // powershell配置文件
   const psProfile = fs.readFileSync(path.join(homeDir, psProfilePath), 'utf-8')
-  const themeNameReg = /(oh-my-posh\sprompt\sinit\spwsh\s--config\s"~\/AppData\/Local\/oh-my-posh\/themes\/)(.*?)("\s\|\sInvoke-Expression)/ // 主题名匹配规则
-  const allThemes = getThemes()
+  const allThemes = fs.readdirSync(process.env.POSH_THEMES_PATH)
 
   const nextTheme = allThemes[Math.floor(Math.random() * allThemes.length)]
-  const newPSProfile = psProfile.replace(themeNameReg, "$1" + nextTheme + "$3"); // 替换主题后的profile文件
+  const newPSProfile = psProfile.replace(/[^\\]*\.omp\.json/, nextTheme); // 替换主题后的profile文件
 
   fs.writeFileSync(path.join(homeDir, psProfilePath), newPSProfile, 'utf-8')
   console.log('设置成功');
